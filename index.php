8900c8430ed3c3075f7d1a44285f55bb573f5095
marcosmallet <marcosmallet@gmail.com>
2016-01-29 23:20:49 -0200

    teste1

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+// string json contendo os dados de um funcionário
+$json_str = '{"nome":"Paulo da Silva", "idade":38, "sexo": "M"}';
+// disponibilizando para consumo 
+echo $json_str; 
+?> 
