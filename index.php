2fa0ec83ca1d1da58c6cf8fda14aa6c858153e90
marcosmallet <marcosmallet@gmail.com>
2016-01-30 09:46:30 -0200

    lista de usuarios populada

--- index.php
+++ index.php
@@ -13,22 +13,42 @@ class Users {
     public $user_photo;
     public $isFollowing;
     public $ranking;
-}
 
-class ListUsers {
-	public $user;
-
-	function __construct() {
-		$this->user = new Users();
-		$this->user->user_id = 1;
-		$this->user->user_name = 'Marcos';	
-		$this->user->user_photo = '/foto/marcos.jpg';
-		$this->user->isFollowing = true;
-		$this->user->ranking = 10;	
-
-		echo json_encode($this->user);	
+    function __construct($id, $name, $photo, $following, $rank) {
+		$this->user_id = $id;
+		$this->user_name = $name;	
+		$this->user_photo = $photo;
+		$this->isFollowing = $following;
+		$this->ranking = $rank;		
 	}
 }
 
-new ListUsers();
+class UsersList {
+	public $user;
+	public $objList = array();
+
+	function __construct() {
+		$this->user = new Users(1, 'Bob', '/photos/bob.png', true, 10);
+		$this->objList[] = $this->user;
+
+		$this->user = new Users(2, 'Jarry', '/photos/jarry.png', true, 22);
+		$this->objList[] = $this->user;
+
+		$this->user = new Users(3, 'Jonathan', '/photos/jonathan.png', false, 27);
+		$this->objList[] = $this->user;
+
+		$this->user = new Users(4, 'Mike', '/photos/mike.png', false, 35);
+		$this->objList[] = $this->user;
+
+		$this->user = new Users(5, 'Suzanne', '/photos/suzzane.png', false, 49);
+		$this->objList[] = $this->user;
+
+		$this->user = new Users(6, 'Tom', '/photos/tom.png', true, 55);
+		$this->objList[] = $this->user;
+
+		echo json_encode($this->objList);	
+	}
+}
+
+new UsersList();
 ?> 
